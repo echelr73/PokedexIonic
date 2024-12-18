b891e6606524cb7259f3b64aecaaf50b00e199af
Martin Echeverria <martin.echeverria@accenture.com>
2024-12-18 19:36:18 -0300

    Add loading

--- src/app/pages/list-pokemons/list-pokemons.page.ts
+++ src/app/pages/list-pokemons/list-pokemons.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 import { Pokemon } from 'src/app/models/pokemon';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
@@ -12,21 +13,43 @@ export class ListPokemonsPage implements OnInit {
   public pokemons: Pokemon[] = [];
   
   constructor(
-    private pokemonService: PokemonService
+    private pokemonService: PokemonService,
+    private loadingController: LoadingController
   ) { }
 
   ngOnInit() {
-    this.motePokemon();
+    this.morePokemon();
   }
 
-  motePokemon(){
+  morePokemon($event = null){
     const promise = this.pokemonService.getPorkemons();
 
     if(promise){
+
+      let loading = null;
+      if (!$event) {
+        loading = this.loadingController.create({
+          message: 'Loading...'
+        });
+        loading.then( (res) => {
+          res.present();
+        });
+      }
+
       promise.then((results) => {
         console.log(results);
         this.pokemons = this.pokemons.concat(results);
         console.log(this.pokemons);
+        if ($event) {
+          $event.target.complete();
+        }
+        loading.then( (res) => {
+          res.dismiss();
+        });
+      }).catch( (err) => {
+        if ($event) {
+          $event.target.complete();
+        }
       });
 
     }
